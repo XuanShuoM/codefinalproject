952c576cb5414a2f8e683b81a7da614d23c867a7
Xinhui Ma <373926@colum.edu>
2023-05-03 17:03:43 -0500

    done

--- temperature/main.js
+++ temperature/main.js
@@ -16,9 +16,11 @@ $('#menu-toggle').click(function(){
   function nightMode() {
     document.getElementById("background").style.backgroundImage = "url(../image/ban.jpeg)";
     document.getElementById("background").style.color = "white";
+    document.getElementById("highlight").style.backgroundColor = "darkblue";
  }
 
  function dayMode() {
   document.getElementById("background").style.backgroundImage = "url(../image/bg.jpg)";
   document.getElementById("background").style.color = "black";
+  document.getElementById("highlight").style.backgroundColor = "lightblue";
 }
